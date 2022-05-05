bf4f3dea1c432f5a355f0eea9e88013b75dce06f
piotoor <piotoor@gmail.com>
2022-05-05 13:43:07 +0200

    Revert "Scrapped synch mechanism. Everything but fps counter is in main loop again"
    This reverts commit b4eda486931799d32bf0c02b03f648a8398a6218.

--- Main.asm
+++ Main.asm
@@ -23,17 +23,16 @@ irq_ticks=$73
 ;;---------------------------------------------
 main            
                 jsr setup
+                lda #0
+                sta synch
 @mainloop
-                
+                lda synch
+                bne @continue
                 jsr compute_frame
-                jsr draw_frame
-                
-                
-        
-                jsr check_keyboard
-
                 inc main_ticks
-                jmp @mainloop
+                lda #1
+                sta synch
+@continue       jmp @mainloop
                 
 ;;---------------------------------------------
 ;; irq
@@ -41,13 +40,17 @@ main
 irq             
                 dec $d019               ; acknowledge IRQ / clear register for next interrupt
                 
-
-                inc irq_ticks
+                lda synch
+                beq frame_not_ready
+                jsr draw_frame
+                jsr check_keyboard
+                lda #0
+                sta synch
+frame_not_ready inc irq_ticks
                 lda irq_ticks
                 cmp #50
-                
                 bne @end
-                lda main_ticks
+@update_fps     lda main_ticks
                 sta $400
                 
                 lda #0
